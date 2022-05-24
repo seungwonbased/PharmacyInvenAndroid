94c58c6df9e57ea9f8828346ae8fdc182897848d
Suah-Cho <sacho0929@gmail.com>
2022-05-25 01:59:41 +0900

    login and register page(not finished)

---
+++ app/src/main/java/com/example/pharmacyinven/JoinActivity.java
@@ -0,0 +1,53 @@
+package com.example.pharmacyinven;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class JoinActivity extends AppCompatActivity {
+    TextView back;
+    EditText name,id,pw,pw2,number;
+    Button pwcheck, submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+
+        //뒤로 가기 버튼
+        back = findViewById(R.id.back);
+        back.setOnClickListener(v -> onBackPressed() );
+
+        //기입 항목
+        name = findViewById(R.id.signName);
+        id=findViewById(R.id.signID);
+        pw=findViewById(R.id.signPW);
+        pw2=findViewById(R.id.signPW2);
+        number = findViewById(R.id.signNumber);
+
+        //비밀번호 확인 버튼
+        pwcheck = findViewById(R.id.pwcheckbutton);
+        pwcheck.setOnClickListener(v -> {
+            if(pw.getText().toString().equals(pw2.getText().toString())){
+                pwcheck.setText("일치");
+            }else{
+                Toast.makeText(JoinActivity.this, "비밀번호가 다릅니다.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //회원가입 완료 버튼
+        submit = findViewById(R.id.signupbutton);
+        submit.setOnClickListener(v -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+
+
+    }
+}
