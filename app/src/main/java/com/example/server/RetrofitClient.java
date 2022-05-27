a1db9fe91cfa7cb14c88e96a7fbdab25e4881887
baeseungwon <21storeo@gmail.com>
2022-05-27 13:03:21 +0900

    ADD Client's API

---
+++ app/src/main/java/com/example/server/RetrofitClient.java
@@ -0,0 +1,31 @@
+package com.example.server;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static RetrofitClient instance = null;
+    private static RetrofitInterface retrofitInterface;
+    private static String baseUrl = "http://10.0.2.2:8080/";
+
+    private RetrofitClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
+    }
+
+    public static RetrofitClient getInstance() {
+        if (instance == null) {
+            instance = new RetrofitClient();
+        }
+        return instance;
+    }
+
+    public static RetrofitInterface getRetrofitInterface() {
+        return retrofitInterface;
+    }
+
+}
