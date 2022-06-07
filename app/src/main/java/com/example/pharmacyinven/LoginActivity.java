032b8510ff95c71b5466a601451f5aa749b36b77
Suah-Cho <sacho0929@gmail.com>
2022-06-08 00:50:00 +0900

    msg

--- app/src/main/java/com/example/pharmacyinven/LoginActivity.java
+++ app/src/main/java/com/example/pharmacyinven/LoginActivity.java
@@ -8,20 +8,11 @@ import android.widget.TextView;
 
 
 public class LoginActivity extends AppCompatActivity {
-    TextView sign;
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //회원가입 버튼
-        sign = findViewById(R.id.signin);
-
-        //회원가입 버튼 클릭시, 회원가입 페이지로 이동
-        sign.setOnClickListener(v -> {
-            Intent intent = new Intent(this, JoinActivity.class);
-            startActivity(intent);
-        });
     }
 }
