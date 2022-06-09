474140de0215db33a097362063f1b585a27bd582
이정호 <leejeongho@192.168.nate.com>
2022-06-09 21:29:49 +0900

    update msg

--- app/src/main/java/com/example/pharmacyinven/JoinActivity.java
+++ app/src/main/java/com/example/pharmacyinven/JoinActivity.java
@@ -11,8 +11,8 @@ import android.widget.Toast;
 
 public class JoinActivity extends AppCompatActivity {
     TextView back;
-    EditText name,id,pw,pw2,number;
-    Button pwcheck, submit;
+    EditText name,id,pw,number;
+    Button  submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +27,7 @@ public class JoinActivity extends AppCompatActivity {
         name = findViewById(R.id.signName);
         id=findViewById(R.id.signID);
         pw=findViewById(R.id.signPW);
-        pw2=findViewById(R.id.signPW2);
         number = findViewById(R.id.signNumber);
-
-        //비밀번호 확인 버튼
-        pwcheck = findViewById(R.id.pwcheckbutton);
-        pwcheck.setOnClickListener(v -> {
-            if(pw.getText().toString().equals(pw2.getText().toString())){
-                pwcheck.setText("일치");
-            }else{
-                Toast.makeText(JoinActivity.this, "비밀번호가 다릅니다.", Toast.LENGTH_LONG).show();
-            }
-        });
 
         //회원가입 완료 버튼
         submit = findViewById(R.id.signupbutton);
@@ -46,8 +35,5 @@ public class JoinActivity extends AppCompatActivity {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         });
-
-
-
     }
 }
